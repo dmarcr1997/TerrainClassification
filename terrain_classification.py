cab12c46490b19463e2cb2f9802b551a60308f70
Damon Marc Rocha II <dmarcr1997@gmail.com>
2025-04-03 01:27:37 -0500

    Model creation and removed unused return value from create_model

--- terrain_classification.py
+++ terrain_classification.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2) # Split into train test data. 20% of data will be test data
     # print(f"X SIZES - Train: {len(X_train)} | Test: {len(X_test)}")
     # print(f"Y SIZES - Train: {len(y_train)} | Test: {len(y_test)}")
-    model, accuracy, cm, report = create_and_train_model(X_train, X_test, y_train, y_test)
+    model, accuracy, cm = create_and_train_model(X_train, X_test, y_train, y_test)
 
     print(f"Accuracy: {accuracy}")
     print("\nConfusion Matrix:")
